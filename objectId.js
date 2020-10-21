abb7f123e5505bd668c11d5efdf52ddcfd545636
Felipe Seda <fsra1610@gmail.com>
2020-10-21 17:17:13 -0300

    added files to .gitignore

--- objectId.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const id = new mongoose.Types.ObjectId();
-console.log(id.getTimestamp());
-console.log(id);
-
-const isValid = mongoose.Types.ObjectId.isValid('5f90769ff1c260b41fca3e03');
-console.log(isValid);
